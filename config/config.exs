9b2af1dc2c4bc536f4239938dd965ff23b8259c3
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-04-17 20:29:04 +0900

    [master] Fix indent

--- config/config.exs
+++ config/config.exs
@@ -3,11 +3,11 @@
 use Mix.Config
 
 repo_parent_dir = Path.expand(Path.join([__DIR__, "..", ".."]))
-  deps_dir =
-    case Path.basename(repo_parent_dir) do
-      "deps" -> repo_parent_dir                                 # this solomon instance project is used by a gear
-      _      -> Path.expand(Path.join([__DIR__, "..", "deps"])) # this solomon instance project is the toplevel mix project
-    end
+deps_dir =
+  case Path.basename(repo_parent_dir) do
+    "deps" -> repo_parent_dir                                 # this solomon instance project is used by a gear
+    _      -> Path.expand(Path.join([__DIR__, "..", "deps"])) # this solomon instance project is the toplevel mix project
+  end
 
 solomon_config_file = Path.join([deps_dir, "solomon", "config", "config.exs"])
 if File.regular?(solomon_config_file) do
