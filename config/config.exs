18d67cd827594947214ede9d175fd648d6781f4d
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-04-17 18:01:16 +0900

    [master] Handle when the project is used as a dependency

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,14 @@
 
 use Mix.Config
 
-solomon_config_file = Path.expand(Path.join([__DIR__, "..", "deps", "solomon", "config", "config.exs"]))
+repo_parent_dir = Path.expand(Path.join([__DIR__, "..", ".."]))
+  deps_dir =
+    case Path.basename(repo_parent_dir) do
+      "deps" -> repo_parent_dir                                 # this solomon instance project is used by a gear
+      _      -> Path.expand(Path.join([__DIR__, "..", "deps"])) # this solomon instance project is the toplevel mix project
+    end
+
+solomon_config_file = Path.join([deps_dir, "solomon", "config", "config.exs"])
 if File.regular?(solomon_config_file) do
   import_config solomon_config_file
 end
