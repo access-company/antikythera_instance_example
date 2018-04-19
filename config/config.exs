00be7eae66864cf5ff6ba2aa1e14d4cea0a342c6
Shunsuke Kirino <shunsuke.kirino@access-company.com>
2018-04-19 13:52:22 +0900

    [142068] Update solomon and use :antikythera_instance_name config key

--- config/config.exs
+++ config/config.exs
@@ -5,8 +5,8 @@ use Mix.Config
 repo_parent_dir = Path.expand(Path.join([__DIR__, "..", ".."]))
 deps_dir =
   case Path.basename(repo_parent_dir) do
-    "deps" -> repo_parent_dir                                 # this solomon instance project is used by a gear
-    _      -> Path.expand(Path.join([__DIR__, "..", "deps"])) # this solomon instance project is the toplevel mix project
+    "deps" -> repo_parent_dir                                 # this antikythera instance project is used by a gear
+    _      -> Path.expand(Path.join([__DIR__, "..", "deps"])) # this antikythera instance project is the toplevel mix project
   end
 
 solomon_config_file = Path.join([deps_dir, "solomon", "config", "config.exs"])
@@ -14,4 +14,6 @@ if File.regular?(solomon_config_file) do
   import_config solomon_config_file
 end
 
-config :solomon, [solomon_instance_name: :solomon_instance_example]
+config :solomon, [
+  antikythera_instance_name: :solomon_instance_example,
+]
