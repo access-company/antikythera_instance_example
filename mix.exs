38e0556e6c40adf914be621d4dd2e8e94e86efd4
AntikytheraDeploy <antikythera-gr@access-company.com>
2023-06-13 02:30:34 +0000

    [CI] Update antikythera

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 # Copyright(c) 2015-2023 ACCESS CO., LTD. All rights reserved.
 
-antikythera_dep = {:antikythera, [github: "access-company/antikythera", ref: "93d306342088b01babbf302cc186947981971a57"]}
+antikythera_dep = {:antikythera, [github: "access-company/antikythera", ref: "185f48edbd58fb59076cce26332b2c26e806167b"]}
 
 try do
   parent_dir = Path.expand("..", __DIR__)
