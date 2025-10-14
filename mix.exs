3447f035593320435f26629259b901e05a4c34e7
Yuya Shiratori <yuya.shiratori@access-company.com>
2025-10-14 16:17:16 +0900

    [313581] Update websocket_client to 1.5.0

--- mix.exs
+++ mix.exs
@@ -68,7 +68,7 @@ try do
         {:exsync          , "0.2.4" , [only: :dev ]},
         {:meck            , "0.8.13", [only: :test]}, # required to run testgear tests
         {:yaml_elixir     , "2.9.0" , [only: :test]}, # required to run testgear tests
-        {:websocket_client, "1.3.0" , [only: :test]}, # required to run testgear tests (also essential for upgrade_compatibility_test)
+        {:websocket_client, "1.5.0" , [only: :test]}, # required to run testgear tests (also essential for upgrade_compatibility_test)
 
         # indirect tool deps
         # yaml_elixir
